505574aacdd755966796fc4a7cc8c3e13a052f62
NurlanPalvanAke <artyom.valeriyevich@gmail.com>
2023-03-23 00:12:01 +0600

    add localStorage for home pages

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,37 +4,39 @@ import axios from "../../axios";
 import {IUser} from "../../type/userItem";
 import {Link} from "react-router-dom";
 import {HiOutlineMagnifyingGlass} from 'react-icons/hi2'
-
 type IItem = {
     name:string
 }
 
-
 const Home = () => {
-
-
-
-    const [contacts,setContacts] = useState([])
-    const [search,setSearch] = useState('')
     useEffect(()=>{
         axios('http://localhost:3000/users')
-            .then(({data}) => setContacts(data))
+            .then(({data}) => localStorage.setItem('users',JSON.stringify(data)))
     },[])
+
+
+    const [search,setSearch] = useState('')
+
+    const myUsersString = localStorage.getItem('users')
+    const myUsers = JSON.parse(myUsersString || '')
 
     return (
         <main className={'home'}>
-
             <div className="container">
-
                 <label className="home__label">
                     <HiOutlineMagnifyingGlass/>
-                    <input value={search} onChange={(e)=> setSearch(e.target.value)} placeholder={`${contacts.length} Контактов`} type="search" className="home__search"/>
+                    <input value={search}
+                           onChange={(e)=> setSearch(e.target.value)}
+                           placeholder={`Поиск контактов`}
+                           type="search"
+                           className="home__search"/>
                 </label>
 
                 <h3 className={'home__title'}>Всё контакты</h3>
                 <div className={'home__row'}>
                 {
-                    contacts.sort((a:{name:string},b:{name:string}) => a.name.localeCompare(b.name))
+                    myUsers
+                        .sort((a:{name:string},b:{name:string}) => a.name.localeCompare(b.name))
                         .filter((item:IItem) =>item.name.toUpperCase().includes(search.toUpperCase()))
                         .map((item:IUser)=>(
                             <div key={item.id} className={'home__contact'}>
